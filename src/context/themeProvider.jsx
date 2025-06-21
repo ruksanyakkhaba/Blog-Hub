c7a000df0d8853df0f83a22b02e35a4dc3188241
Gopal Bhandari <gopalbhandari056@gmail.com>
2025-06-21 19:37:37 +0545

    made changes

--- src/context/themeProvider.jsx
+++ src/context/themeProvider.jsx
@@ -1,14 +1,33 @@
-import { createContext, useState } from "react";
+import { createContext, useState,useEffect } from "react";
 
  
  const ThemeContext = createContext();
   const ThemeProvider = ({children}) =>{
-    const[darkMode,setDarkMode] = useState(false)
-    const toogleDarkMode = ()=>{
+    const[darkMode,setDarkMode] = useState(true)
+    const toggleDarkMode = ()=>{
+        console.log("Clicked")
         setDarkMode(!darkMode)
     }
+
+ useEffect(() => {
+    if (darkMode) {
+      document.body.classList.add('dark');
+      localStorage.setItem('darkMode', 'true');
+    } else {
+      document.body.classList.remove('dark');
+      localStorage.setItem('darkMode', 'false');
+    }
+  }, [darkMode]);
+
+
+  useEffect(() => {
+    const storedDarkMode = localStorage.getItem('darkMode');
+    if (storedDarkMode) {
+      setDarkMode(storedDarkMode === 'true');
+    }
+  }, []);
     return (
-        <ThemeContext.Provider value={{darkMode,toogleDarkMode}} >
+        <ThemeContext.Provider value={{darkMode,toggleDarkMode}} >
             {children}
         </ThemeContext.Provider>
     )
